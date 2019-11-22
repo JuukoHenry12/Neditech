66f6c9bd4e1a9fd055a93791a7f2b91d5024d06d
Juuko Henry <juukohenry23@gmail.com>
2019-11-22 12:49:15 +0300

    adding project

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+
+var typed3 = new Typed('.typed', {
+   strings:
+    ['We build Experience that Engages',
+     ],
+    
+  typeSpeed: 60,
+  backSpeed: 60,
+  smartBackspace: true, // this is a default
+  loop: true
+});
